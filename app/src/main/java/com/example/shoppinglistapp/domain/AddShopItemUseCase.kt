90acec7e36a5a66643c4091c6254511f1ca486bb
RorschachGi <tumanovAccount7773@rambler.ru>
2023-06-17 14:36:45 +0400

    Creation: data, domain, presentation layers Creating a ShopItem Data Class Description of UseCases: GetShopListUseCase(), GetShopItemUseCase(), AddShopItemUseCase(), DeleteShopItemUseCase(), EditShopItemUseCase()
    update: create interface ShopListRepository

--- app/src/main/java/com/example/shoppinglistapp/domain/AddShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglistapp/domain/AddShopItemUseCase.kt
@@ -1,9 +1,9 @@
 package com.example.shoppinglistapp.domain
 
-class AddShopItemUseCase {
+class AddShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
     fun addShopItem(shopItem: ShopItem){
-        TODO()
+        shopListRepository.addShopItem(shopItem)
     }
 
 }
