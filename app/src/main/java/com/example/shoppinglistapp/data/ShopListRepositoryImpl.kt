0694f25f3d406ee1cb336a1dfa4ea2bde7f1a43a
RorschachGi <tumanovAccount7773@rambler.ru>
2023-06-18 16:47:46 +0400

    create MainViewModel in presentation layer change return type to GetShopListUseCase to LiveData create method updateList() in ShopListRepositoryImpl

--- app/src/main/java/com/example/shoppinglistapp/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglistapp/data/ShopListRepositoryImpl.kt
@@ -1,23 +1,36 @@
 package com.example.shoppinglistapp.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.shoppinglistapp.domain.ShopItem
 import com.example.shoppinglistapp.domain.ShopListRepository
 
 object ShopListRepositoryImpl: ShopListRepository {
 
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
+
     private val shopList = mutableListOf<ShopItem>()
 
     private var autoIncrementId = 0
+
+    init{
+        for(i in 0 until 10){
+            val item = ShopItem("Name $i", i, true)
+            addShopItem(item)
+        }
+    }
 
     override fun addShopItem(shopItem: ShopItem) {
         if(shopItem.id == ShopItem.UNDEFINED_ID){
             shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
     override fun editShopItem(shopItem: ShopItem) {
@@ -32,8 +45,12 @@ object ShopListRepositoryImpl: ShopListRepository {
         } ?: throw java.lang.RuntimeException("Element with id $shopItemId not found")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+
+    private fun updateList(){
+        shopListLD.value = shopList.toList()
     }
 
 }
---
+++ app/src/main/java/com/example/shoppinglistapp/presentation/MainViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.shoppinglistapp.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglistapp.data.ShopListRepositoryImpl
+import com.example.shoppinglistapp.domain.DeleteShopItemUseCase
+import com.example.shoppinglistapp.domain.EditShopItemUseCase
+import com.example.shoppinglistapp.domain.GetShopListUseCase
+import com.example.shoppinglistapp.domain.ShopItem
+
+class MainViewModel: ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+
+    private val deleteShopListUseCase = DeleteShopItemUseCase(repository)
+
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = getShopListUseCase.getShopList()
+
+
+    fun deleteShopItem(shopItem: ShopItem){
+        deleteShopListUseCase.deleteShopItem(shopItem)
+    }
+
+    fun changeEnabledState(shopItem: ShopItem){
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+    }
+
+}
